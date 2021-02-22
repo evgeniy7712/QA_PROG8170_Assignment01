9984a62b59c6d4620c986b5d904b070c5474d5d7
Yevhen <zinovkinevgenij@MacBook-Pro-Zinovkin.local>
2021-02-21 22:54:41 -0500

    my assignment

---
+++ midd/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace midd
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter length: ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width: ");
+            int w = Convert.ToInt32(Console.ReadLine());
+            Rectangle r = new Rectangle(l, w);
+
+            bool done = false;
+            while (!done)
+            {
+                Console.WriteLine("Case 1 get length");
+                Console.WriteLine("Case 2 set length");
+                Console.WriteLine("Case 3 get width");
+                Console.WriteLine("Case 4 set length");
+                Console.WriteLine("Case 5 get parameter");
+                Console.WriteLine("Case 6 get area");
+                Console.WriteLine("Case 7 exit");
+                int action = Convert.ToInt32(Console.ReadLine());
+                switch (action)
+                {
+                    case 1:
+                        Console.WriteLine("Length = "+r.GetLength());
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter new length:");
+                        r.SetLength(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 3:
+                        Console.WriteLine("Width = "+r.GetWidth());
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter new width");
+                        r.SetWidth(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 5:
+                        Console.WriteLine("Perimeter = "+r.GetPerimeter());
+                        break;
+                    case 6:
+                        Console.WriteLine("Area = "+r.GetArea());
+                        break;
+                    case 7:
+                        return;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ midd/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace midd
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+        public Rectangle(int _length, int _width)
+        {
+            length = _length;
+            width = _width; 
+        }
+        public int GetLength() {
+            return length;
+        }
+        public int SetLength(int _length)
+        {
+            Console.WriteLine("Enter new length");
+            length =_length;
+            return 0;
+        }
+        public int GetWidth() {
+            return width;
+        }
+        public int SetWidth(int _width)
+        {
+            Console.WriteLine("Enter new width");
+            width =_width;
+            return 0;
+        }
+        public int GetPerimeter() {
+            return (width+length)*2;
+        }
+        public int GetArea() {
+            return width*length;
+        }
+        
+    }
+}
